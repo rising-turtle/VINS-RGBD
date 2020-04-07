0a0bf8ec99c5dbbb99246710af3a211479a1371c
He Zhang <hxzhang1@ualr.edu>
2020-04-06 23:51:45 -0400

    add sr4000

--- vins_estimator/src/estimator_node_ss.cpp
+++ vins_estimator/src/estimator_node_ss.cpp
@@ -362,6 +362,7 @@ int main(int argc, char **argv)
     ROS_WARN("waiting for image and imu...");
 
     registerPub(n);
+    nG = G.z(); 
 
     ros::Subscriber sub_imu = n.subscribe(IMU_TOPIC, 2000, imu_callback, ros::TransportHints().tcpNoDelay());
     ros::Subscriber sub_image = n.subscribe("/feature_tracker/feature", 2000, feature_callback);
